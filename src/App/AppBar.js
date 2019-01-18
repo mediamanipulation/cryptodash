ab6347aac950eae427c50e51614ec719916454be
mediamanipulation <manipulation2000@hotmail.com>
2019-01-17 23:50:58 -0600

    code

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -10,7 +10,7 @@ grid-template-columns: 180px auto 100px 100px ;
 const ControlButtonElem = styled.div`
 cursor: pointer;
 ${props => props.active && css`
-    color: blue;
+    text-shadow: 0px 0px 60px #03ff03;
     `}
 `
 function ControlButton({name, active }){
