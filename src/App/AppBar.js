7873d6ab43c59a9e48413fc55a9ad6834bcdbdb6
mediamanipulation <manipulation2000@hotmail.com>
2019-01-18 06:18:50 -0600

    Added Context AppProvider

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import styled, {css}from 'styled-components';
+import styled, {css} from 'styled-components';
+import {AppContext} from './AppProvider';
+
 const Logo = styled.div`
 font-size: 1.5em;
 `
@@ -18,18 +20,22 @@ function toProperCase(lower){
     return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
-function ControlButton({name, active }){
+function ControlButton({name}){
     return (
-        <ControlButtonElem active={active}>
-            {toProperCase(name)}
-        </ControlButtonElem>
+        <AppContext.Consumer>
+        {({page}) => (
+            <ControlButtonElem active={page === name}>
+                {toProperCase(name)}
+            </ControlButtonElem>
+        )}
+        </AppContext.Consumer>
         )
 }
 export default function(){
     return( <Bar>
         <Logo>CryptoDash</Logo>
         <div></div>
-        <ControlButton active name='dashboard'/>
+        <ControlButton name='dashboard'/>
         <ControlButton name='settings'/>
     </Bar>
     )
