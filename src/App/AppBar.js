a5cf94a87b62666c9e8481cf8fa1f4e2a40aa9b3
mediamanipulation <manipulation2000@hotmail.com>
2019-01-17 23:59:05 -0600

    functions for case

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -6,17 +6,22 @@ font-size: 1.5em;
 const  Bar = styled.div`
 display: grid;
 grid-template-columns: 180px auto 100px 100px ;
+margin-bottom: 40px;
 `
 const ControlButtonElem = styled.div`
 cursor: pointer;
 ${props => props.active && css`
-    text-shadow: 0px 0px 60px #03ff03;
+    text-shadow: 0px 0px 30px #03ff03;
     `}
 `
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
 function ControlButton({name, active }){
     return (
         <ControlButtonElem active={active}>
-            {name}
+            {toProperCase(name)}
         </ControlButtonElem>
         )
 }
@@ -24,8 +29,8 @@ export default function(){
     return( <Bar>
         <Logo>CryptoDash</Logo>
         <div></div>
-        <ControlButton active name='Dashboard'/>
-        <ControlButton name='Settings'/>
+        <ControlButton active name='dashboard'/>
+        <ControlButton name='settings'/>
     </Bar>
     )
 }
